1499ec2755d6ac11b8c316c445f4a4ae9ff531ee
fercho23 <fernandoarielmateos@gmail.com>
2015-07-08 14:47:20 -0300

    Role Seeder
    Asignación de permisos a los roles.

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -11,10 +11,10 @@ class RoleTableSeeder extends Seeder {
         DB::table('roles')->truncate();
 
         $role = Role::create(array(//1
-                'description'   => '',
-                'name'          =>'Super Usuario',
-                'slug'          =>'super_user'
-                ));
+                    'description'   => '',
+                    'name'          =>'Super Usuario',
+                    'slug'          =>'super_user'
+                    ));
         $role->permissions()->sync([1, 2, 3, 4, 5,
                                     6, 7, 8, 9, 10,
                                     11, 12, 13, 14, 15, 16,
@@ -24,35 +24,63 @@ class RoleTableSeeder extends Seeder {
                                     37, 38, 39, 40, 41, 42,
                                     ]);
 
-        Role::create(array(//2
-                'description'   => '',
-                'name'          =>'Director',
-                'slug'          =>'director'
-                ));
+        $role = Role::create(array(//2
+                    'description'   => '',
+                    'name'          =>'Director',
+                    'slug'          =>'director'
+                    ));
+        $role->permissions()->sync([1, 2, 3, 4, 5,
+                                    6, 7, 8, 9, 10,
+                                    11, 12, 13, 14, 15, 16,
+                                    17, 18, 19, 20, 21,
+                                    22, 23, 24, 25, 26,
+                                    27, 28, 29, 30, 31, 32, 33, 34, 35, 36,
+                                    37, 38, 39, 40, 41, 42,
+                                    ]);
 
-        Role::create(array(//3
-                'description'   => '',
-                'name'          =>'Recepcion y Conserjeria',
-                'slug'          =>'reception_janitor'
-                ));
 
-        Role::create(array(//4
-                'description'   => '',
-                'name'          =>'Mantenimiento, servicios técnicos y seguridad:',
-                'slug'          =>'maintenance'
-                ));
+        $role = Role::create(array(//3
+                    'description'   => '',
+                    'name'          =>'Recepcion y Conserjeria',
+                    'slug'          =>'reception_janitor'
+                    ));
+        $role->permissions()->sync([6, 7, 8, 10,
+                                    12, 15,
+                                    18, 21,
+                                    23, 26,
+                                    27, 28, 29, 31, 32, 33, 34, 35, 36,
+                                    37, 38, 39, 40, 41, 42,
+                                    ]);
 
-        Role::create(array(//5
-                'description'   => '',
-                'name'          =>'Cliente',
-                'slug'          =>'client'
-                ));
+        $role = Role::create(array(//4
+                    'description'   => '',
+                    'name'          =>'Pisos',
+                    'slug'          =>'cleaning'
+                    ));
+        $role->permissions()->sync([12, 15,
+                                    37, 38, 39, 40, 41, 42,
+                                    ]);
 
-        Role::create(array(//6
-                'description'   => '',
-                'name'          =>'Pisos',
-                'slug'          =>'cleaning'
-                ));
+        $role = Role::create(array(//5
+                    'description'   => '',
+                    'name'          =>'Mantenimiento, servicios técnicos y seguridad:',
+                    'slug'          =>'maintenance'
+                    ));
+        $role->permissions()->sync([12, 15,
+                                    37, 38, 39, 40, 41, 42,
+                                    ]);
+
+        $role = Role::create(array(//6
+                    'description'   => '',
+                    'name'          =>'Cliente',
+                    'slug'          =>'client'
+                    ));
+        $role->permissions()->sync([12, 15,
+                                    18, 21,
+                                    27, 28, 33, 34, 35, 36,
+                                    ]);
+
+
     }
 
 }
