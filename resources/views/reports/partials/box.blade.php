e389e84738fb301544f8aff42a0aecf4cc808dba
Emmanuel <emmanuel.sf@hotmail.com>
2015-08-13 00:28:01 -0300

    Mejoras en Pantalla Reports

--- resources/views/reports/partials/box.blade.php
+++ resources/views/reports/partials/box.blade.php
@@ -1,8 +1,12 @@
 <div class="{!!$class!!}">
 <span class="info-box-icon"><i class="{!!$icon!!}"></i></span>
 	<div class="info-box-content">
-		<span class="info-box-text">{!!$reportTitle!!}<span>
-		<span class="info-box-number"><a class="create" comparefield="{!!$compareField!!}" form="{!!$form!!}" reporttype="{!!$reporttype!!}" reporttitle="{!!$reportTitle!!}" href="javascript:void(0)">GENERAR</a></span>
+		<span class="info-box-text">{!!$reporttitle!!}<span>
+		<span id="{!!$reporttype!!}" class="info-box-number">
+			<input type="hidden" id="comparefield" value="{!!$comparefield!!}">
+			<input type="hidden" id="reporttitle" value="{!!$reporttitle!!}">
+			<a class="create" reporttype="{!!$reporttype!!}" href="javascript:void(0)">GENERAR</a>
+		</span>
 		<hr style="margin:0; color:grey;">
 		<span class="progress-description">
 		{!!$reportDescription!!}
---
+++ resources/views/reports/partials/form.blade.php
@@ -0,0 +1,39 @@
+<!-- Form Entre Fechas -->
+  <div id="form" class="box box-danger">
+	<div class="box-header with-border">
+	  <h3 id="form-title" class="box-title">Ingrese fechas para generar el reporte</h3>
+	</div>
+	<div class="box-body">
+	  <div class="row">
+		{!! Form::open(['url'=>'reports/generate']) !!}
+		{!! Form::hidden('reporttype', '', array('id' => 'reporttype')) !!}
+		{!! Form::hidden('reporttitle', '', array('id' => 'reporttitle')) !!}
+		{!! Form::hidden('comparefield', '', array('id' => 'comparefield')) !!}
+		<div class="col-xs-4" id="select-room">	
+			<label>Habitación</label>
+			<div class="input-group">			    
+			    {!! Form::select('room_id', $rooms, null, ['class'=>'form-control']) !!}
+			</div>
+		</div>
+		<div class="col-xs-4" id="select-firstdate">
+			<label>Fecha de Inicio</label>
+			<div class="input-group">				
+				<span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+				{!! Form::input('date','firstDate', null, ['class'=>'form-control']) !!}
+			</div>
+		</div>
+		<div class="col-xs-4" id="select-seconddate">
+			<label>Fecha de Fin</label>
+			<div class="input-group">
+				<span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+				{!! Form::input('date','secondDate', null, ['class'=>'form-control']) !!}
+			</div>
+		</div>
+		<div class="col-xs-4">
+			<label> </label>
+		  	{!! Form::submit("Enviar", ['class'=>'btn btn-block btn-primary']) !!}
+		</div>
+		{!! Form::close() !!}
+	  </div>
+	</div><!-- /.box-body -->
+  </div>
