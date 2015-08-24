eef7b4112be07ede0570eb80a984cc905bd5c708
Emmanuel <emmanuel.sf@hotmail.com>
2015-08-24 15:41:46 -0300

    Fon awesome

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,7 @@
         <!-- Bootstrap 3.3.2 -->
         <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- FontAwesome 4.4.0 -->
-        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- Theme style -->
         <link href="{{ asset('/dist/css/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
         <!-- AdminLTE Skins. Choose a skin from the css/skins
